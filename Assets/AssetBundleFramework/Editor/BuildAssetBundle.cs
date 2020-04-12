47eb2f7558db456ff77d0d6509bbbbcb05031688
setFalcon <1449530193@qq.com>
2020-04-12 16:21:11 +0800

    完成删除功能,editor分支基本完成

--- Assets/AssetBundleFramework/Editor/BuildAssetBundle.cs
+++ Assets/AssetBundleFramework/Editor/BuildAssetBundle.cs
@@ -7,9 +7,12 @@ namespace AssetBundleFramework.Editor {
     /// <summary>
     /// 对标记的资源进行AssetBundle打包
     /// </summary>
-    public class BuildAssetBundle {
-        [MenuItem("AssetBundleTools/Build All AssetBundles")]
-        public static void BuildAssetBundles() {
+    public static class BuildAssetBundle {
+        /// <summary>
+        /// 通用的打包方法
+        /// </summary>
+        /// <param name="target">要打包的目标平台</param>
+        private static void BuildAssetBundles(BuildTarget target) {
             // Ab包的输出路径
             string abOutputPath = PathTools.GetAbOutputPath();
             // 检查Asset/ 下是否存在StreamingAssetsPath
@@ -17,8 +20,26 @@ namespace AssetBundleFramework.Editor {
                 Directory.CreateDirectory(abOutputPath);
             }
 
-            //进行打包
-            BuildPipeline.BuildAssetBundles(abOutputPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneLinux64);
+            // 进行打包
+            BuildPipeline.BuildAssetBundles(abOutputPath, BuildAssetBundleOptions.None, target);
+            // 刷新Asset/
+            AssetDatabase.Refresh();
         }
-    }// Class_End
-}// Namespace_End
+
+        /// <summary>
+        /// Linux平台的打包方法
+        /// </summary>
+        [MenuItem("AssetBundleTools/Build All AssetBundles/Linux", false, 20)]
+        public static void BuildAssetBundlesLinux() {
+            BuildAssetBundles(BuildTarget.StandaloneLinux64);
+        }
+
+        /// <summary>
+        /// Windows平台的打包方法
+        /// </summary>
+        [MenuItem("AssetBundleTools/Build All AssetBundles/Windows", false, 20)]
+        public static void BuildAssetBundlesWindows() {
+            BuildAssetBundles(BuildTarget.StandaloneWindows64);
+        }
+    } // Class_End
+} // Namespace_End
--- Assets/AssetBundleFramework/Editor/DeleteAssetBundle.cs
+++ Assets/AssetBundleFramework/Editor/DeleteAssetBundle.cs
@@ -1,8 +1,25 @@
+using System.IO;
+using AssetBundleFramework.Tools;
+using UnityEditor;
+
 namespace AssetBundleFramework.Editor {
     /// <summary>
     /// 删除已经打包的Ab包文件
     /// </summary>
-    public class DeleteAssetBundle {
-        
+    public static class DeleteAssetBundle {
+        /// <summary>
+        /// 批量删除已经打包的Ab包文件
+        /// </summary>
+        [MenuItem("AssetBundleTools/Delete All AssetBundle", false, 40)]
+        public static void DeleteAllAssetBundle() {
+            // Ab包的输出目录
+            string delDir = PathTools.GetAbOutputPath();
+            if (!string.IsNullOrEmpty(delDir)) {
+                // 删除路径下的所有文件和*.meta文件
+                Directory.Delete(delDir, true);
+            }
+            // 刷新Asset/
+            AssetDatabase.Refresh();
+        }
     }
 }
