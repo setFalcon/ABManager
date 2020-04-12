df289114d14a4c4a88a8e82b3ad1ce0df45b76a8
setFalcon <1449530193@qq.com>
2020-04-12 18:00:38 +0800

    创建加载单一Ab包的类

---
+++ Assets/AssetBundleFramework/SingleAssetBundleLoader.cs
@@ -0,0 +1,10 @@
+namespace AssetBundleFramework {
+    /// <summary>
+    /// 使用WWW/UnityWebRequestAssetBundle加载单一AssetBundle
+    /// </summary>
+    public class SingleAssetBundleLoader : System.IDisposable {
+        public void Dispose() {
+            
+        }
+    }
+}
