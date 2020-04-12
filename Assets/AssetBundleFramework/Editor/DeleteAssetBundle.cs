3faf344b4b86abc14def5a70f400fba562f8c373
setFalcon <1449530193@qq.com>
2020-04-12 15:44:07 +0800

    提交,为合并做准备

--- Assets/AssetBundleFramework/Editor/DeleteAssetBundle.cs
+++ Assets/AssetBundleFramework/Editor/DeleteAssetBundle.cs
@@ -2,5 +2,7 @@ namespace AssetBundleFramework.Editor {
     /// <summary>
     /// 删除已经打包的Ab包文件
     /// </summary>
-    public class DeleteAssetBundle { }
+    public class DeleteAssetBundle {
+        
+    }
 }
